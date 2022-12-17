890b5c11875dcd6b6fa68fc6c4f728cea8d67cf4
Gol4m <73995699+Gol4m@users.noreply.github.com>
2022-12-17 13:38:26 +0300

    ai 2.0

--- ai.py
+++ ai.py
@@ -3,30 +3,26 @@ import math as m
 from PIL import Image, ImageDraw
 
 MAX_ITERATIONS = 75
-zr = 0
 
 
 def convert_in_ones(Y):
-    one = 1
-    negative_one = -1
     #
     Y_new = Y
     for n in range(len(Y_new[0])):
         if 0 <= Y[0][n][0]:
-            Y_new[0][n][0] = one + zr
+            Y_new[0][n][0] = 1
         else:
-            Y_new[0][n][0] = negative_one + zr
+            Y_new[0][n][0] = -1
     return Y_new
 
 
-def if_equal(Y, X, one):
-    zero = 0
+def if_equal(Y, X):
     for j in range(len(X)):
         k = 0
         for i in range(len(Y[0])):
-            if Y[zero][i][zero] == X[j][i][zero]:
-                k = k + one + zr
-        if k == len(Y[zero]):
+            if Y[0][i][0] == X[j][i][0]:
+                k = k + 1
+        if k == len(Y[0]):
             return True
     return False
 
@@ -34,14 +30,14 @@ def if_equal(Y, X, one):
 def draw_image(path, Y, SIZE):
     image = Image.new('1', (SIZE, SIZE), "white")
     draw = ImageDraw.Draw(image)
-    index = 0 + zr
+    index = 0
     for i in range(SIZE):
         for j in range(SIZE):
             if Y[index][0] < 0:
-                clr = 0 + zr
+                clr = 0
             else:
-                clr = 1 + zr
-            index = index + 1 + zr
+                clr = 1
+            index = index + 1
             draw.point((i, j), clr)
     image.save(path)
     image.show()
@@ -49,22 +45,32 @@ def draw_image(path, Y, SIZE):
 
 def activation_function(Y):
     for i in range(len(Y)):
-        for j in range(len(Y[i + zr])):
-            Y[i + zr][j + zr] = th(Y[i + zr][j + zr])
+        for j in range(len(Y[i])):
+            Y[i][j] = th(Y[i][j])
     return Y
 
 
+def sh(x):
+    return m.exp(x) - m.exp(-x)
+
+
+def ch(x):
+    return m.exp(x) + m.exp(-x)
+
+
 def th(x):
-    one = 1
-    two = 2
-    return (m.exp(two*x) - one + zr) / (m.exp(two*x) + one + zr)
+    return sh(x) / ch(x)
+
+
+def transparent(X):
+    return X.T
 
 
 def get_weights(X, SIZE):
     W = np.zeros((SIZE**2, SIZE**2))
     for n in range(len(X)):
-        ups = (W @ X[n + zr] - X[n + zr]) @ (W @ X[n + zr] - X[n + zr]).T
-        W = W + (ups / (X[n + zr].T @ X[n + zr] - X[n + zr].T @ W @ X[n + zr]))
+        ups = (W @ X[n] - X[n]) @ transparent(W @ X[n] - X[n])
+        W = W + (ups / (transparent(X[n]) @ X[n] - transparent(X[n]) @ W @ X[n]))
     return W
 
 
@@ -80,27 +86,22 @@ def get_image(img_path, SIZE):
         for i in range(SIZE):
             for j in range(SIZE):
                 if pixels[i, j][0] == 255:
-                    required_pixel = one + zr
+                    required_pixel = one
                 else:
                     required_pixel = negative_one
                 x[index] = required_pixel
-                index = index + one + zr
+                index = index + one
         X.append(x)
     return X
 
 
-if __name__ == "__main__":
-    one = 1
-    SIZE = 30
-    img_path = ["normal_images/0.png", "normal_images/1.png",
-                "normal_images/2.png", "normal_images/3.png",
-                "normal_images/4.png", "normal_images/5.png",
-                "normal_images/6.png", "normal_images/7.png",
-                "normal_images/8.png", "normal_images/9.png"]
-
+def get_image_and_weights(img_path, SIZE):
     X = get_image(img_path, SIZE)
     W = get_weights(X, SIZE)
+    return X, W
 
+
+def while_cycle(X, W, SIZE):
     input_image = input("Введите название цифры которую будет распозновать: ")
     input_image_show = Image.open("curved_images/%s.png" % input_image)
     print("Открываю картинку цифры %s.." % input_image)
@@ -116,11 +117,28 @@ if __name__ == "__main__":
             break
         Y = [activation_function((W @ Y[0]))]
         Y_new = convert_in_ones(Y)
-        if if_equal(Y_new, X, one):
+        if if_equal(Y_new, X):
             print("Распознал!")
             print("Восстановленное изображение: image_result/img.png")
             draw_image(f"output_images/img.png", Y[0], SIZE)
             flag = False
-        iteration = iteration + one + zr
+        iteration = iteration + 1
         pass
+
+
+def main():
+    SIZE = 30
+    img_path = ["normal_images/0.png", "normal_images/1.png",
+                "normal_images/2.png", "normal_images/3.png",
+                "normal_images/4.png", "normal_images/5.png",
+                "normal_images/6.png", "normal_images/7.png",
+                "normal_images/8.png", "normal_images/9.png"]
+
+    X, W = get_image_and_weights(img_path, SIZE)
+
+    while_cycle(X, W, SIZE)
+
+
+if __name__ == "__main__":
+    main()
 
